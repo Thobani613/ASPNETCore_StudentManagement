47b9481237deb8a902f0e72f0506a09f0d81950b
Thobani613 <thobanimjiyakho7@gmail.com>
2026-05-25 21:53:54 +0200

    Initial commit: ASP.NET Core MVC Student Management System

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,165 @@
+﻿using ASPNETCore_DB.Interfaces;
+using ASPNETCore_DB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCore_DB.Controllers
+{
+    [TypeFilter(typeof(CustomExceptionFilter))]
+    public class StudentController : Controller
+    {
+        private readonly IStudent _studentRepo;
+        public StudentController(IStudent studentRepo)
+        {
+            try
+            {
+                _studentRepo = studentRepo;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Constructor not initialized - IStudent studentRepo");
+            }
+            
+        }
+        public IActionResult Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
+        {
+            pageNumber = pageNumber ?? 1;
+            int pageSize = 3;
+
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["StudentNumberSortParm"] = String.IsNullOrEmpty(sortOrder) ? "number_desc" : "";
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else 
+            { 
+                searchString = currentFilter; 
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            ViewResult viewResult =  View();
+
+            try
+            {
+                viewResult = View(PaginatedList<Student>.Create(_studentRepo.GetStudents(searchString, sortOrder).AsNoTracking(), pageNumber ?? 1, pageSize));
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("No student records detected");
+            }
+                        
+            return viewResult;
+        }
+        public IActionResult Details(string id)
+        {
+            ViewResult viewDetail = View();
+            try
+            {
+                viewDetail = View(_studentRepo.Details(id));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Student detail not found");
+            }
+
+
+            return viewDetail;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("StudentNumber, FirstName, Surname, EnrollmentDate")] Student student)
+ {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _studentRepo.Create(student);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Student could not be created");
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            ViewResult viewDetail = View();
+            try
+            {
+                viewDetail = View(_studentRepo.Details(id));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Student detail not found");
+            }
+            return viewDetail;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _studentRepo.Edit(student);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Student detail could not be edited");
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            ViewResult viewDetail = View();
+            try
+            {
+                viewDetail = View(_studentRepo.Details(id));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Student detail not found");
+            }
+            return viewDetail;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([Bind("StudentNumber, FirstName, Surname, EnrollmentDate")] Student student)
+        {
+            try 
+            {
+                _studentRepo.Delete(student);
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Student could not be deleted");
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using ASPNETCore_DB.Data;
+using ASPNETCore_DB.Interfaces;
+using ASPNETCore_DB.Models;
+using ASPNETCore_DB.Repositories;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SQLiteDBContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IExceptionFilter, CustomExceptionFilter>();
+builder.Services.AddScoped<IDBInitializer, DBInitializerRepo>();
+builder.Services.AddScoped<IStudent, StudentRepo>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
